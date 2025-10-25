091492488254339ac3d4ca7dd8caf22fe160cd70
Paritosh Kiran Gandre <paritoshkrcg@gmail.com>
2025-10-24 22:20:22 -0400

    Echohealth project +

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Publications from "./components/Publications.jsx";
 import Footer from "./components/Footer.jsx";
 import { nav, education } from "./data.js";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   // Theme persistence
@@ -67,6 +68,7 @@ export default function App() {
       </main>
       <Footer />
       <Analytics/>
+      <SpeedInsights/>
     </>
   );
 }
